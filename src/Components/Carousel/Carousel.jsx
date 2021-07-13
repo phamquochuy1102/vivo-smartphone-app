139a3403ad20bd124abe12ab0ef85caf038e4715
Quoc Huy <pqhuy0211@gmail.com>
2021-07-13 13:53:21 +0700

    carousel component and update navbar

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Carousel.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from "react-bootstrap/Carousel";
+
+const CarouselComponent = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect} interval={2000}>
+        <Carousel.Item>
+          <picture className="intro__background">
+            <source
+              media="(max-width:767px)"
+              srcset="https://www.vivosmartphone.vn/uploads/home/banner-Y72-vertical%20.jpg"
+            />
+            <img src="./assets/img/slideshow-1.jpg" alt="VIVO" />
+          </picture>
+        </Carousel.Item>
+        <Carousel.Item>
+          <picture className="intro__background">
+            <source
+              media="(max-width:767px)"
+              srcset="https://www.vivosmartphone.vn/uploads/home/Banner-ban-do-yeu-thuong-1080-1957.jpg"
+            />
+            <img src="./assets/img/slideshow-2.jpg" alt="VIVO" />
+          </picture>
+        </Carousel.Item>
+        <Carousel.Item>
+          <picture className="intro__background">
+            <source
+              media="(max-width:767px)"
+              srcset="https://www.vivosmartphone.vn/uploads/MANGOADS/banner/x60pro_vertical.jpg"
+            />
+            <img src="./assets/img/slideshow-3.jpg" alt="VIVO" />
+          </picture>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CarouselComponent;
--- src/Containers/Homepage.jsx
+++ src/Containers/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Carousel from "../Components/Carousel/Carousel";
 import Navbar from "../Components/Navbar/Navbar";
 
 const Homepage = () => {
   return (
     <div>
       <Navbar />
+      <Carousel />
     </div>
   );
 };
