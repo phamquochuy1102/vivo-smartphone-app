2e5a33500f8121807c58da9115863f19e0c21dd6
Quoc Huy <pqhuy0211@gmail.com>
2021-07-14 20:51:17 +0700

    explore component

--- src/Containers/Homepage.jsx
+++ src/Containers/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "../Components/Carousel/Carousel";
+import Explore from "../Components/Explore/Explore";
 import Navbar from "../Components/Navbar/Navbar";
 import Technology from "../Components/Technology/Technology";
 import TypeSeries from "../Components/TypeSeries/TypeSeries";
@@ -11,6 +12,7 @@ const Homepage = () => {
       <Carousel />
       <TypeSeries />
       <Technology />
+      <Explore />
     </div>
   );
 };
